7691c94fe0e09367172833a5077c302f1d521fb4
Faizeennn <muhammad1802ft.2020@student.uny.ac.id>
2023-05-15 07:51:04 +0700

    add level selection panel

---
+++ Assets/Scripts/OptionPanel.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionPanel : MonoBehaviour
+{
+    [SerializeField] AudioManager audioManager;
+    [SerializeField] Toggle muteToggle;
+    [SerializeField] Slider bgmSlider;
+    [SerializeField] Slider sfxSlider;
+    [SerializeField] TMP_Text bgmVolText;
+    [SerializeField] TMP_Text sfxVolText;
+
+    private void Start() {
+        muteToggle.isOn = audioManager.IsMute;
+        bgmSlider.value = audioManager.BGMVolume;
+        sfxSlider.value = audioManager.SFXVolume;
+        SetBgmVolText(bgmSlider.value);
+        SetSfxVolText(sfxSlider.value);
+    }
+
+    public void SetBgmVolText(float value)
+    {
+        bgmVolText.text = Mathf.RoundToInt(value * 100).ToString();
+    }
+
+    public void SetSfxVolText(float value)
+    {
+        sfxVolText.text = Mathf.RoundToInt(value * 100).ToString();
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("bgmVolume", audioManager.BGMVolume);
+        PlayerPrefs.SetFloat("sfxVolume", audioManager.SFXVolume);
+        PlayerPrefs.SetInt("mute", audioManager.IsMute ? 1 : 0);
+    }
+}
